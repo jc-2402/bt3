c03f1b77a789a4975fbf2e3831d11a0b7c66de30
Jiya Chowdhury <138868637+jc-2402@users.noreply.github.com>
2024-09-02 23:19:13 +0530

    Add files via upload

---
+++ BT18.java
@@ -0,0 +1,52 @@
+//transform to sumtree
+import java.util.*;
+public class BT18{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static int transform(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftchild = transform(root.left);
+        int rightchild = transform(root.right);
+
+        int data = root.data;
+        int newleft = root.left == null ? 0 : root.left.data;
+        int newright = root.right == null ? 0 : root.right.data;
+
+        root.data = newleft + leftchild + newright + rightchild;
+        return data;
+    }
+    public static void preorder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        //System.out.println(transform(root));
+        preorder(root);
+        System.out.println();
+        System.out.println("after transforming to sum tree");
+        transform(root);
+        preorder(root);
+    }
+}
